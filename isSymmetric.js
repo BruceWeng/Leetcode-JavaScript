7228a0b5f954efebaffbbee22c5a798aecb9a626
BruceWeng <weng8916@gmail.com>
2016-09-08 18:05:05 -0700

    BFS

---
+++ isSymmetric.js
@@ -0,0 +1,70 @@
+function TreeNode(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+function isSymmetric(root) {
+  if (root === null) {
+    return true;
+  }
+
+  let queue = new Array();
+  queue.push(root);
+
+  while(queue.length !== 0) {
+    let level = new Array();
+    let size = queue.length;
+
+    for (let i = 0; i < size; i++) {
+      let head = queue.shift();
+
+      if (head === null) {
+        level.push(null);
+        continue;
+      } else {
+        level.push(head.val);
+      }
+
+      queue.push(head.left);
+
+      queue.push(head.right);
+    }
+
+    for (let i = 0; i <= Math.floor(size); i++) {
+      if (level[i] !== level[size - 1 - i]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+let test1 = new TreeNode(1);
+test1.left = new TreeNode(2);
+test1.right = new TreeNode(2);
+test1.left.left = new TreeNode(3);
+test1.left.right = new TreeNode(4);
+test1.right.left = new TreeNode(4);
+test1.right.right = new TreeNode(3);
+
+console.log(isSymmetric(test1)); //true
+
+let test2 = new TreeNode(1);
+test2.left = new TreeNode(2);
+test2.right = new TreeNode(2);
+test2.left.left = new TreeNode(null);
+test2.left.right = new TreeNode(3);
+test2.right.left = new TreeNode(null);
+test2.right.right = new TreeNode(3);
+
+console.log(isSymmetric(test2)); //false
+
+let test3 = new TreeNode(3);
+
+console.log(isSymmetric(test3)); //true
