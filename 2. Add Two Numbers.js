e06a0139467b20796cda96883fc808bdfc9eecb7
Bruce Weng <weng8916@gmail.com>
2018-07-11 18:28:11 -0700

    2. Add Two Numbers.js

--- 2. Add Two Numbers.js
+++ 2. Add Two Numbers.js
@@ -1,4 +1,26 @@
 /**
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+ */
+/**
+ * Algorithm: Two pointer
+ * 0. Declare dummy = new ListNode(0); current = dummy;
+ * 1. Declare two pointers val1 and val2 to track values for l1 and l2
+ * 2. Declare carry to store the tenth digit of sum(val1 + val2 + carry)
+ * 3. Move current.next = new ListNode(sum % 10)
+ * 4. Move current = current.next
+ * 5. return dummy.next
+ */
+/**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
@@ -8,21 +30,21 @@ function addTwoNumbers(l1, l2) {
         return null;
     }
 
-    let result = new ListNode(0);
-    let current = result;
+    let dummy = new ListNode(0);
+    let current = dummy;
     let val1 = 0;
     let val2 = 0;
     let carry = 0;
 
-    while (l1 || l2) {
-        if (l1) {
+    while (l1 !== null || l2 !== null) {
+        if (l1 !== null) {
             val1 = l1.val;
             l1 = l1.next;
         } else {
             val1 = 0;
         }
 
-        if (l2) {
+        if (l2 !== null) {
             val2 = l2.val;
             l2 = l2.next;
         } else {
@@ -39,6 +61,5 @@ function addTwoNumbers(l1, l2) {
         current.next = new ListNode(carry);
     }
 
-    return result.next;
-
+    return dummy.next;
 }
