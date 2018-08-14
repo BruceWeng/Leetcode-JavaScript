4b9b746e9630f7175c029f01ee60169c2ec7bdd0
Bruce Weng <weng8916@gmail.com>
2018-08-14 11:57:14 -0700

    BinarySearch.js

--- BinarySearch.js
+++ BinarySearch.js
@@ -37,6 +37,35 @@ const binarySearch = function(nums, target) {
     return checkCount;
 }
 
+/**
+ * Solution 2
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  
+    let start = 0;
+    let end = nums.length - 1;
+    
+    while (start <= end) { 
+        
+      let mid = start + Math.floor((end - start) / 2);
+          
+      if (nums[mid] === target) return mid;
+        
+      if (nums[mid] > target) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+        
+    return - 1;
+};
+
 let test = [1, 2, 3, 4, 5, 6, 7, 8];
 let target1 = 0;
 let target2 = 4;
