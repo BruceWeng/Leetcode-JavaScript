dca8205e7a8a181aae058c19b922735c190c9975
Bruce Weng <weng8916@gmail.com>
2018-10-12 22:57:07 -0700

    Update readFileSync 'utf8'

--- JS read file example/readFileSync.js
+++ JS read file example/readFileSync.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
-let words = fs.readFileSync(process.argv[2]).toString();
+let words = fs.readFileSync(process.argv[2], 'utf8').toString();
 words = words.split('\n');
 console.log(words);
