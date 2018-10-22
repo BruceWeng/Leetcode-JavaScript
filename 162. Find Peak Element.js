d6ba40be5d383e2a0234f128471f861a87734f26
Bruce Weng <weng8916@gmail.com>
2018-10-22 14:28:48 -0700

    162. Find Peak Element.js

--- 162. Find Peak Element.js
+++ 162. Find Peak Element.js
@@ -79,3 +79,32 @@ console.log(findPeakElementIteration(test1)); // 2
 
 let test2 = [1,2,1,3,5,6,4];
 console.log(findPeakElementIteration(test2)); // 1 or 5
+
+/**
+ * Leetcode Fundamental: 10/22 Update
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    // use start and end as the range boundary for binary search [start...end] (included)
+    // while true case: if ther are two elements return the larger one, thus true candition can not be start <= end 
+    // since we will miss the larger element by get next mid = start + (end - start) // 2 (ex: 1 + (2-1) // 2 = 1 and nums[2] is the larger one)
+    // true condition: while start + 1 < end, we gonna handle the range of two out of the while loop
+    let start = 0;
+    let end = nums.length - 1;
+    while (start + 1 < end) { // there is always at least two elements and the mid will be the later one
+      let mid = start + Math.floor((end - start) / 2);
+      // handle true case: nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]
+      if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+      // handle the false case where nums[mid] < nums[mid+1]: mid would not be candidate, start = mid+1
+      else if (nums[mid] < nums[mid+1]) start = mid + 1;
+      // handle the false case where nums[mid] < nums[mid-1]: mid would not be candidate, end = mid-1
+      else end = mid - 1;
+    }
+    
+    // handle the case that the range contains only two elements: return the larger one
+    if (nums[end] > nums[start]) return end;
+    else return start;
+  };
