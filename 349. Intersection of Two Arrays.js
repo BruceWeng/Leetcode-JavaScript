651bba946a8281719a7d403c88e8697f126d2f6f
Bruce Weng <weng8916@gmail.com>
2018-11-07 15:30:51 -0800

    349. Intersection of Two Arrays.js

--- 349. Intersection of Two Arrays.js
+++ 349. Intersection of Two Arrays.js
@@ -23,3 +23,26 @@ let intersection = function(nums1, nums2) {
 
 console.log(intersection([1,2,2,1], [2,2])); // [2]
 console.log(intersection([4,9,5], [9,4,9,8,4])); // [9,4]
+
+/**
+ * Leetcode Fundamental: 11/7 Update
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  // handle edge case
+  if (nums1 === undefined || nums2 === undefined || nums1.length === 0 || nums2.length === 0) return [];
+  
+  let result = [];
+  let set1 = new Set(nums1);
+  let set2 = new Set(nums2);
+  
+  for (let key of set1) {
+    if (set2.has(key)) result.push(key);
+  }
+  
+  return result;
+};
