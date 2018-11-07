2ac56e662b2129f791bfd8a19b88780620bafcad
Bruce Weng <weng8916@gmail.com>
2018-11-07 15:24:31 -0800

    3. Longest Substring Without RepeatingCharacters.js

--- 209. Minimum Size Subarray Sum.js
+++ 209. Minimum Size Subarray Sum.js
@@ -34,3 +34,40 @@ let s = 7;
 let nums = [2,3,1,2,4,3];
 console.log(minSubArrayLen(s, nums)); // 2
 // Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+
+/**
+ * Leetcode Fundamental: 11/7 Update
+ * Failure:
+ * 1. Fail to declare a result(default = len+1) to update minimum sum
+ * 2. Fail to think of inner while loop true condition sum >= s
+ * (Jump into inner while loop to update result)
+ * 3. Fail to update result = Math.min(result, end - start + 1)
+ */
+/**
+ * @param {number} s
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(s, nums) {
+  // Handle edge case
+  if (nums === undefined || nums.length === 0 || s === undefined) return 0;
+  // let start be first index of substring and end be the last index of substring (inclusive)
+  let start = 0;
+  let end = -1; // gonna move and count sum
+  let sum = 0;
+  let result = nums.length + 1;
+  while (end < nums.length) {
+    end += 1;
+    sum += nums[end];
+    
+    while (sum >= s) {
+      result = Math.min(result, end - start + 1);
+      sum -= nums[start];
+      start += 1;
+    }
+  }
+  
+  // Handle no match case
+  if (result === nums.length + 1) return 0;
+  return result;
+};
--- 3. Longest Substring Without RepeatingCharacters.js
+++ 3. Longest Substring Without RepeatingCharacters.js
@@ -30,3 +30,39 @@ var lengthOfLongestSubstring = function(s) {
 
   return maxLen;
 };
+/**
+ * Leetcode Fundamental: 11/7 Update
+ * Failure:
+ * 1. Fail to think of store end index in map as value
+ * 2. Fail to update start by map[nums[end]] + 1
+ * 3. Fail to think of the timing to update start
+ * 
+ * T: One Pass O(n)
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    // Handle edge case
+    if (s === undefined || s.length === 0) return 0;
+    
+    // Declare a hash table with key: character, value: end index in substring range: [start...end] (inclusive)
+    let start = 0;
+    let map = {};
+    let result = 0;
+    for (let end = 0; end < s.length; end += 1) {
+      // 2 thing to do no matter what in this loop
+      // 1. reset map[s[end]] = end
+      // 2. result = Math.max(result, len)
+      
+      // When we find current char is in map(repeated):
+      // Move start pointer to end + 1 if end + 1 > start
+      if (s[end] in map) start = Math.max(start, map[s[end]] + 1);
+      
+      map[s[end]] = end;
+      result = Math.max(result, end - start + 1);
+    }
+    
+    return result;
+  };
