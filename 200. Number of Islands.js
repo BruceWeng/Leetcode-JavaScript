01fdbde97328dec2899926eaa6690c94c2ec4bf9
Bruce Weng <weng8916@gmail.com>
2018-11-21 15:03:34 -0800

    200. Number of Islands.js

--- 200. Number of Islands.js
+++ 200. Number of Islands.js
@@ -76,7 +76,62 @@ const test1 = [ ['1', '1', '0', '0', '0'],
                 ['1', '1', '0', '0', '0'],
                 ['0', '0', '1', '0', '0'],
                 ['0', '0', '0', '1', '1'] ];
-// console.log(numIslands(test1)); // 3
 
 const test2 = [["1","1","1"],["0","1","0"],["1","1","1"]]
-console.log(numIslands(test2)); // 1
+
+/**
+ * Leetcode Fundamental: 11/21 Update
+ * 
+ * Flood Fill
+ * Failure:
+ * 1. Fail to wrap recursive call in if (grid[i][j] === '1')
+ *    Recursive call should handle cases from all inputs
+ *    Should not depends on condition in for loops
+ * 
+ * T: O(mn)
+ * 
+ * Runtime: 68 ms
+ */
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands2 = function(grid) {
+  if (grid === undefined || grid.length === 0 || grid[0].length === 0) return 0;
+
+  let m = grid.length;
+  let n = grid[0].length;
+
+  let result = 0;
+  for (let i = 0; i < m; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+      if (grid[i][j] === '1') {
+        result += 1;
+        findConnected(grid, i, j); // grid easy to typo
+      }
+    }
+  }
+
+  return result;
+};
+
+const findConnected = (grid, i, j) => {
+  if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] === '2') return;
+
+  // check for recursion call:
+  // 1. if cell is 1: mark visited cell as 2
+  // 2. recursive call
+  if (grid[i][j] === '1') {
+    grid[i][j] = '2';
+
+    let dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]; // up right down left
+    for (let d of dirs) {
+      let nextI = i + d[0];
+      let nextJ = j + d[1];
+      findConnected(grid, nextI, nextJ);
+    }
+  }
+};
+
+console.log(numIslands2(test1)); // 3
+console.log(numIslands2(test2)); // 1
