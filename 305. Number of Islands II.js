dc87a02817405e899da67e1d716f61bcc9499648
Bruce Weng <weng8916@gmail.com>
2018-11-21 17:22:33 -0800

    305. Number of Islands II.js

--- 305. Number of Islands II.js
+++ 305. Number of Islands II.js
@@ -18,7 +18,7 @@ Can you do it in time complexity O(k log mn), where k is the length of the posit
  * @param {number[][]} positions
  * @return {number[]}
  */
-const numIslands2 = function(m, n, positions) {
+const numIslands = function(m, n, positions) {
   let result = [];
   if (m <= 0 || n <= 0) return result;
 
@@ -81,4 +81,105 @@ const find = function(id, roots) {
 let m = 3;
 let n = 3;
 let positions = [[0,0], [0,1], [1,2], [2,1]];
+
+/**
+ * Leetcode Fundamental: 11/21 Update
+ * Union Find T: Nearly O(1)
+ * T: O(k) or O(klogmn)
+ * Runtime: 136 ms
+ * Optimization: Union by rank
+ */
+function Island(m, n) {
+  var groups = new Array(m*n).fill(-1);
+  var ranks = new Array(m*n).fill(0);
+  var count = 0;
+
+  const getCount = () => {
+    return count;
+  };
+  
+  const getGroups = (i, j, col) => {
+    let index = i * col + j
+    return groups[index];
+  };
+
+  const add = (i, j, col) => {
+    let index = i * col + j;
+    groups[index] = index;
+    ranks[index] = 1;
+    count += 1;
+  };
+  
+  const find = (index) => {
+    // Isolated
+    if (groups[index] === index) return index;
+  
+    // Group
+    return find(groups[index]);
+  };
+  
+  const union = (i1, j1, i2, j2, col) => {
+    let index1 = i1 * col + j1;
+    let index2 = i2 * col + j2;
+    let root1 = find(index1);
+    let root2 = find(index2);
+  
+    // Already unioned
+    if (root1 === root2) return;
+  
+    // Union by Rank:
+    // groups[smallRoot] = largeRoot
+    // ranks[largeRoot] += ranks[smallRoot]
+    // rank1 > rank2: swap roots
+    if (ranks[root1] > ranks[root2]) [[root1, root2] = [root2, root1]];
+
+    // Union by Rank
+    groups[root1] = root2;
+    ranks[root2] += ranks[root1];
+
+    count -= 1;
+  };
+  
+  return {
+    add,
+    find,
+    union,
+    getCount,
+    getGroups
+  }  
+}
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} positions
+ * @return {number[]}
+ */
+const numIslands2 = function(m, n, positions) {
+  if (m === undefined || n === undefined || positions === undefined ||
+      m === 0 || n === 0 || positions.length === 0 || positions[0].length < 2)
+      return [];
+
+  let result = [];
+
+  let island = Island(m, n);
+  let dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]; // up right down left
+  for (let p of positions) {
+    let i = p[0];
+    let j = p[1];
+    // Add island
+    island.add(i, j, n);
+
+    // Union neighbor
+    for (let d of dirs) {
+      let nextI = i + d[0];
+      let nextJ = j + d[1];
+      if (nextI >= 0 && nextI < m && nextJ >= 0 && nextJ < n && island.getGroups(nextI, nextJ, n) !== -1) island.union(i, j, nextI, nextJ, n);
+    }
+
+    // Update count
+    result.push(island.getCount());
+  }
+  return result;
+};
+
 console.log(numIslands2(m, n, positions)); // [1,1,2,3]
