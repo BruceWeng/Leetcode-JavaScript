e7f89d0e43f947e228c4743617d412d8209107f4
Bruce Weng <weng8916@gmail.com>
2019-01-21 15:12:40 -0800

    202. Happy Number.js

--- 202. Happy Number.js
+++ 202. Happy Number.js
@@ -33,3 +33,38 @@ function getNextNumber(n) {
 
     return sum;
 }
+
+/**
+ * Leetcode Fundamental: Set, Update 1/21/2019
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    // 1. Use sum of each digits as key of the set
+    // 2. Use while loop keep adding new sum to the set
+    // 3. If set.has(sum) -> infinite loop, exit while loop, return false
+    // 4. If sum === 1, return true
+    let set = new Set();
+    let sum = calSum(n);
+    if (sum === 1) return true;
+    
+    while (!set.has(sum)) {
+      set.add(sum);
+      sum = calSum(sum);
+      if (sum === 1) return true;
+    }
+    
+    return false;
+  };
+  
+  const calSum = (n) => {
+    let sum = 0;
+    
+    while (n / 10 !== 0) { // no more next quotient
+      sum += (n % 10) * (n % 10);
+      n = parseInt(n / 10);
+    }
+    return sum;
+  }
