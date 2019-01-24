a884223e6c4fd8501f58cca4f3329f7aa2002ba7
Bruce Weng <weng8916@gmail.com>
2019-01-24 15:03:09 -0800

    716. Max Stack.js

--- 716. Max Stack.js
+++ 716. Max Stack.js
@@ -1,30 +1,3 @@
-/**
-Design a max stack that supports push, pop, top, peekMax and popMax.
-
-push(x) -- Push element x onto stack.
-pop() -- Remove the element on top of the stack and return it.
-top() -- Get the element on the top.
-peekMax() -- Retrieve the maximum element in the stack.
-popMax() -- Retrieve the maximum element in the stack, and remove it. If you find more than one maximum elements, only remove the top-most one.
-Example 1:
-MaxStack stack = new MaxStack();
-stack.push(5); 
-stack.push(1);
-stack.push(5);
-stack.top(); -> 5
-stack.popMax(); -> 5
-stack.top(); -> 1
-stack.peekMax(); -> 5
-stack.pop(); -> 1
-stack.top(); -> 5
-Note:
--1e7 <= x <= 1e7
-Number of operations won't exceed 10000.
-The last four operations won't be called when stack is empty.
- */
-/**
- * Leetcode Fundamental: 1/24/2019 Update
- */
 function MaxStack() {
   this.max = Number.MIN_SAFE_INTEGER;
   this.stack = [];
@@ -68,8 +41,10 @@ function MaxStack() {
     // Use another stack to store all elements , which pushed after max
     let tempStack = [];
     let lastIdx = stack.length - 1;
-    while (stack.length !== 0 && stack[lastIdx] !== this.max) 
+    while (stack.length !== 0 && stack[lastIdx] !== this.max) {
       tempStack.push(stack.pop());
+      lastIdx = stack.length - 1; // Gonna update lastIdx because the stack length changed
+    }
     
     let result = this.max;
 
@@ -82,3 +57,9 @@ function MaxStack() {
     return result;
   }
 }
+
+let maxStack = new MaxStack();
+maxStack.push(5);
+maxStack.push(1);
+console.log(maxStack.popMax());
+console.log(maxStack.peekMax());
