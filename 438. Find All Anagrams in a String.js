1924a0244cc143d139b53f06cc630f6298b09431
Bruce Weng <weng8916@gmail.com>
2019-02-10 00:38:37 -0800

    438. Find All Anagrams in a String.js

--- 438. Find All Anagrams in a String.js
+++ 438. Find All Anagrams in a String.js
@@ -76,3 +76,42 @@ The substring with start index = 0 is "ab", which is an anagram of "ab".
 The substring with start index = 1 is "ba", which is an anagram of "ab".
 The substring with start index = 2 is "ab", which is an anagram of "ab".
  */
+
+/**
+* Leetcode Fundamental: 2/10/2019 Update
+*/
+const findAnagrams = (s, p) => {
+  let result = [];
+  if (p.length > s.length) return result;
+
+  let map = new Map();
+  for (let char of p) {
+    if (map.get(char) === undefined) map.set(char, 1);
+    else map.set(char, map.get(char) + 1);
+  }
+
+  let counter = map.size;
+  let start = 0;
+  let end = 0;
+
+  while (end < s.length) {
+    let endChar = s[end];
+    if (map.has(endChar)) {
+      map.set(endChar, map.get(endChar) - 1);
+      if (map.get(endChar) === 0) counter -= 1;
+    }
+    end += 1;
+
+    while (counter === 0) {
+      let startChar = s[start];
+      if (map.has(startChar)) {
+        map.set(startChar, map.get(startChar) + 1);
+        if (map.get(startChar) > 0) counter += 1;
+      }
+
+      if (end - start === p.length) result.push(start);
+      start += 1;
+    }
+  }
+  return result;
+};
