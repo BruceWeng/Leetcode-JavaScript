3705fe3353e9e79f8b3a10d75dfa137cf8728c52
Bruce Weng <weng8916@gmail.com>
2019-02-19 16:11:37 -0800

    Throttle.js

--- Throttle.js
+++ Throttle.js
@@ -1,17 +1,18 @@
 /**
- * Throttle: we don't allow to our function to execute more than once 
+ * Throttle: we don't allow to our function to execute more than once
  * every X milliseconds.
- * 
- * The main difference between this and debouncing is that throttle 
- * guarantees the execution of the function regularly, at least every 
+ *
+ * The main difference between this and debouncing is that throttle
+ * guarantees the execution of the function regularly, at least every
  * X milliseconds.
- * 
+ *
  * Example: Infinite scrolling
  */
 /**
  * Throttle
- * 
+ *
  * @param {Function} fn (not necessary be pure function)
+ * @param {number} limit ms
  * @return {Function} with a timeout for limit ms delay
  */
 const throttle = function(fn, limit) {
@@ -21,19 +22,16 @@ const throttle = function(fn, limit) {
   return (...args) => {
     // 3. Check if the function is not waiting (function not in session)
     if (!waiting) {
-    //a. Change the this in fn to current context and execute on args array
+      //a. Change the this in fn to current context and execute on args array
       fn.apply(this, args);
-    //b. Set waiting to true
+      //b. Set waiting to true
       waiting = true;
-    //c. Set waiting to false after limit ms
-      setTimeout(
-        () => {
-          waiting = false;
-        },
-        limit
-      );
+      //c. Set waiting to false after limit ms
+      setTimeout(() => {
+        waiting = false;
+      }, limit);
     }
 
     // 4. If the function is waiting, do nothing
-  }
-}
+  };
+};
