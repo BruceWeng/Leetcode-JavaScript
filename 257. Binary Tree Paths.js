d362dd8d40b56f8f8b6ebc0da8b0dff8062be6ed
Bruce Weng <weng8916@gmail.com>
2019-11-23 17:35:22 -0800

    257. Binary Tree Paths

--- 257. Binary Tree Paths.js
+++ 257. Binary Tree Paths.js
@@ -35,24 +35,24 @@ Explanation: All root-to-leaf paths are: 1->2->5, 1->3
  * @param {TreeNode} root
  * @return {string[]}
  */
-var binaryTreePaths = function(root) {
+var binaryTreePaths = function (root) {
   if (root === undefined || root === null) return [];
-  
+
   let result = [];
   let stack = [Command(root, String(root.val))];
-  
+
   while (stack.length !== 0) {
     let currCmd = stack.pop();
     let leftNode = currCmd.node.left;
     let rightNode = currCmd.node.right;
-    
+
     if (leftNode === null && rightNode === null) result.push(currCmd.path);
-    
-    if (rightNode !== null) stack.push( Command(rightNode, currCmd.path + "->" + String(rightNode.val)) );
-                      
-    if (leftNode !== null) stack.push( Command(leftNode, currCmd.path + "->" + String(leftNode.val)) );
+
+    if (rightNode !== null) stack.push(Command(rightNode, currCmd.path + "->" + String(rightNode.val)));
+
+    if (leftNode !== null) stack.push(Command(leftNode, currCmd.path + "->" + String(leftNode.val)));
   }
-  
+
   return result;
 };
 
@@ -66,15 +66,15 @@ function Command(node, path) {
 /**
  * Recursive solution
  */
-var binaryTreePaths = function(root) {
+var binaryTreePaths = function (root) {
   if (root === undefined || root === null) return [];
-  
+
   let result = [];
-  
+
   let path = String(root.val);
-  
+
   traverse(root, path, result);
-  
+
   return result;
 };
 
@@ -83,8 +83,41 @@ function traverse(node, path, result) {
     result.push(path);
     return;
   }
-  
+
   if (node.left !== null) traverse(node.left, path + "->" + String(node.left.val), result);
-  
+
   if (node.right !== null) traverse(node.right, path + "->" + String(node.right.val), result);
 }
+
+/**
+ * 2019/11/23 Revisit
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function (root) {
+  let result = [];
+  if (root === null) return result;
+  dfs(root, "");
+  return result;
+
+  function dfs(node, path) {
+    if (node === null) return;
+    if (node.left === null && node.right === null) {
+      result.push(path + node.val);
+    }
+
+    path += node.val + '->';
+
+    dfs(node.left, path);
+    dfs(node.right, path);
+  }
+};
