12dd91705b8726b8e9bcc6515e65ef063ddbe423
Bruce Weng <weng8916@gmail.com>
2020-08-12 11:52:18 -0700

    update 2darray 2020

--- 2darray.js
+++ 2darray.js
@@ -1,6 +1,5 @@
-'use strict';
-var array = [];
-for (let i = 0; i < 5; i++) {
-   array[i] = new Array(5).fill(0);
-}
-console.log(array);
+// Pass by value
+let grid = Array(5).fill(null).map(() => Array(4).fill(0));
+for (let i = 0; i < 5; i += 1) grid[i][0] = 1;
+for (let j = 0; j < 4; j += 1) grid[0][j] = 1;
+console.log(grid);
