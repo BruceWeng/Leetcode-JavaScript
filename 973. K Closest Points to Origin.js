5dc4ebdf790c4ecaa03b6275c7d7e4b350ed5c24
Bruce Weng <weng8916@gmail.com>
2020-09-24 00:37:00 -0700

    973. to be implemented

---
+++ 973. K Closest Points to Origin.js
@@ -0,0 +1,7 @@
+/**
+ * Three solutions to this classical K-th problem.
+ * 1. Sorting
+ * 2. Max-heap
+ * 3. Quick select
+ * https://leetcode.com/problems/k-closest-points-to-origin/discuss/220235/Java-Three-solutions-to-this-classical-K-th-problem.
+ */
