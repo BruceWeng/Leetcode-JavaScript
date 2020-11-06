92fc34ead95d125ec676133ec4a2b7db2b3ab952
Bruce Weng <weng8916@gmail.com>
2020-11-05 16:58:08 -0800

    2020 update

--- 454. 4Sum II.js
+++ 454. 4Sum II.js
@@ -20,7 +20,7 @@ The two tuples are:
 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
  */
 /**
- * Leetcode Fundamental: 11/7 Update
+ * Leetcode Fundamental: 2019/11/7 Update
  * Failure:
  * 1. Fail to think of use sum of c and d as key and freq as value in map
  * 2. Fail to times -1 to sum of a and b
@@ -34,9 +34,9 @@ The two tuples are:
  * @param {number[]} D
  * @return {number}
  */
-var fourSumCount = function(A, B, C, D) {
+var fourSumCount = function (A, B, C, D) {
   // handle edge case
-  
+
   // Construct map: key: sum of each value of C and D, value: count 
   // worst case: each sum is unique: S: O(n^2)
   let map = {};
@@ -47,12 +47,42 @@ var fourSumCount = function(A, B, C, D) {
       if (sumCD in map) map[sumCD] += 1;
       else map[sumCD] = 1;
     }
-  
+
   for (let numA of A)
     for (let numB of B) {
       let sumAB = (numA + numB) * -1; // Remember to times -1 here!
-      if ( sumAB in map) result += map[sumAB];
+      if (sumAB in map) result += map[sumAB];
     }
-  
+
+  return result;
+};
+
+// 2020/11/5 Update
+// let map(sum of a and b, count)
+// T: O(n^2)
+// S: O(n^2), worst case: all a + b are different
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @param {number[]} C
+ * @param {number[]} D
+ * @return {number}
+ */
+var fourSumCount = function (A, B, C, D) {
+  let map = new Map();
+  let result = 0;
+  for (let a of A) {
+    for (let b of B) {
+      let sum = a + b;
+      if (!map.has(sum)) map.set(sum, 1);
+      else map.set(sum, map.get(sum) + 1);;
+    }
+  }
+  for (let c of C) {
+    for (let d of D) {
+      let sum = c + d;
+      if (map.has(-sum)) result += map.get(-sum);
+    }
+  }
   return result;
 };
