62aaf7cab7b8e7a956697d1d7a9f6624b11b4491
Bruce Weng <weng8916@gmail.com>
2021-06-23 17:55:41 -0700

    add test case

--- Nearest City(Amazon OA).js
+++ Nearest City(Amazon OA).js
@@ -8,7 +8,14 @@
  *  3. INTEGER_ARRAY y
  *  4. STRING_ARRAY q
  */
-
+/**
+ * 
+ * @param {string[]} c 
+ * @param {number[]} x 
+ * @param {number[]} y 
+ * @param {string[]} q 
+ * @returns 
+ */
 function closestStraightCity(c, x, y, q) {
     // Write your code here
     const xToPoint = {}
@@ -65,3 +72,8 @@ function getDist(query, string, pointToIdx, x, y) {
     let stIdx = pointToIdx[string]
     return Math.abs(x[qIdx]-x[stIdx])+Math.abs(y[qIdx]-y[stIdx])
 }
+
+console.log(closestStraightCity(["p1","p2","p3"], [30, 20, 10], [30, 20, 30], ["p3", "p2", "p1"]))
+// ["p1", NONE, "p3"]
+console.log(closestStraightCity(["p1", "p2","p3", "p4", "p5"], [10, 20, 30, 40, 50], [10, 20, 30, 40, 50], ["p1", "p2", "p3", "p4", "p5"]))
+// [NONE, NONE, NONE, NONE, NONE]
